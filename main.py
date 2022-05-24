a65260c9b0d31ced74773098f086b287d08b9d5e
Tobias Schmidt <8149@htl.rennweg.at>
2022-05-24 14:34:05 +0200

    automatic fetching from db, building snmp querys

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 __author__ = "Hiermann Alexander, Schmidt Tobias"
 __version__ = 0.1
+
 # pip install psycopg2
 
 import os
@@ -17,40 +18,64 @@ connection = psycopg2.connect(
 )
 
 
-if __name__ == "__main__":
-    sql = """Insert into snmp_query(oid, ip, version, usage)
-    VALUES ('iso.3.2.3.5432.12.6', 345668, 1, 'interface usage');
-    """
+def request_snmp():
     cur = connection.cursor()
+    try:
+        cur.execute('SELECT * FROM snmp_query')
+        for job in cur.fetchall():
+            snmp_query = build_snmp_query(job)
+            if snmp_query:
+                print(snmp_query)
+                execute_snmp_query(snmp_query)
+            # push_snmp_to_db()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if cur is not None:
+            cur.close()
+        if connection is not None:
+            connection.close()
+
+
+def build_snmp_query(row: list):
+    version = row[8]
+    if version == 2:
+        oid = row[1]
+        ip = row[2]
+        username = row[3]
+        if oid and ip and username:
+            return "snmpwalk -v2c -c", username, ip, oid
+        else:
+            print("missing values for version 2 for Object with id=", row[0], sep='')
+    if version == 3:
+        oid = row[1]
+        ip = row[2]
+        username = row[3]
+        encry_meth = row[4]
+        encry_pass = row[5]
+        auth_meth = row[6]
+        auth_pass = row[7]
+        if oid and ip and username and encry_meth and encry_pass and auth_meth and auth_pass:
+            return ("snmpwalk -v3 -l authPriv -a", auth_meth, " -A", auth_pass,
+                    "-x", encry_meth, "-X", encry_pass, "-u", username, ip, oid)
+        else:
+            print("missing values for version 3 for Object with id=", row[0], sep='')
+
+
+def execute_snmp_query(query: str):
+    pass
+
+
+def push_snmp_to_db(response: str, row: list):
+    pass
+
+
+if __name__ == "__main__":
+    sql = """Insert into snmp_query(oid, ip, version, usage, username)
+    VALUES ('iso.4.5.6.7.123.4', 123456, 2, 'Storage usage', 'admin');
+    """
+    #cur = connection.cursor()
     #cur.execute(sql)
     #connection.commit()
-    cur.execute('SELECT * FROM snmp_query')
-    response = cur.fetchall()
-    print(str(response).split(',')[1])
-    cur.close()
-
-
-def request_snmp():
-    build_snmp_query()
-    execute_snmp_query()
-    push_snmp_to_db()
-
-
-def build_snmp_query(row:list):
-    oid = row[1]
-    ip = row[2]
-    username = row[3]
-    encry_meth = row[4]
-    encry_pass = row[5]
-    auth_meth = row[6]
-    auth_pass = row[7]
-    version = row[8]
-
-
-
-def execute_snmp_query(query:str):
-    pass
-
-
-def push_snmp_to_db(response:str, row:list):
-    pass
+    #connection.close()
+    request_snmp()
