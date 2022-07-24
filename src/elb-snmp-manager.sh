06973b67f88c41d60b2ead308d778833fb7ef5af
Alexander Hiermann <8108@htl.rennweg.at>
2022-07-24 17:25:09 +0200

    Added shell script for service with logging

---
+++ src/elb-snmp-manager.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+
+# Set env
+source ../.env
+
+# Logging
+echo "[$(date +"%Y-%m-%dT%H:%M:%S%z")] STARTED SNMP-MANAGER" >> "$LOGFILEDIR/snmp-manager-$(date +"%Y-%m-%d").log"
+
+# Execute python script
+python3 /main.py
